00e765d7e56950d3b05591232007aeaac9a19d3f
ahmar-husain <ahmar.husain@maersk.com>
2022-11-13 10:08:18 +0530

    Set permisions

--- Test/keyvault.tf
+++ Test/keyvault.tf
@@ -3,9 +3,60 @@ resource "azurerm_key_vault" "example" {
   location                    = var.infra_location
   resource_group_name         = azurerm_resource_group.nucleotidz_resource_group.name
   enabled_for_disk_encryption = true
-  tenant_id                   = "eaf4a4cd-97f4-477a-8d21-50da33ef2a64"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   sku_name = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey",
+    ]
+
+    secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set",
+    ]
+
+    storage_permissions = [
+       "backup",
+    "delete",
+    "deletesas",
+    "get",
+    "getsas",
+    "list",
+    "listsas",
+    "purge",
+    "recover",
+    "regeneratekey",
+    "restore",
+    "set",
+    "setsas",
+    "update",
+    ]
+  }
   }
