1452dc7e19d04b8dfc798f00efeab1a6634e870d
ahmar-husain <ahmar.husain@maersk.com>
2022-11-13 10:30:44 +0530

    Destroy

--- Test/variable.tf
+++ Test/variable.tf
@@ -15,3 +15,19 @@ type=string
 variable "object_id"{
 type=string
 }
+
+variable sql_server_name {
+  type = string
+}
+
+variable integration_db_sku_name {
+  type = string
+}
+
+variable integration_db_max_size_gb {
+  type = string
+}
+
+variable integration_db_zone_redundant {
+  type = string
+}
