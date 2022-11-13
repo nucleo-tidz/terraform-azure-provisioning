e2d24f2e26c4e7d149d87cca3cbb1a03dc520106
ahmar-husain <ahmar.husain@maersk.com>
2022-11-13 10:27:19 +0530

    destroy

--- Test/keyvault.tf
+++ Test/keyvault.tf
@@ -1,4 +1,4 @@
-resource "azurerm_key_vault" "example" {
+resource "azurerm_key_vault"   "example" {
   name                        = var.keyvault_name
   location                    = var.infra_location
   resource_group_name         = azurerm_resource_group.nucleotidz_resource_group.name
